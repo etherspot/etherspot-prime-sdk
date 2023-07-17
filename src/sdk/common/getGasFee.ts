273b53b5beaec4a112a54e0e7a6c5f5108678468
marie-fourier <sobirovutkir@gmail.com>
2023-07-17 20:14:09 +0500

    fix pre-eip1559 gas price

--- src/sdk/common/getGasFee.ts
+++ src/sdk/common/getGasFee.ts
@@ -6,21 +6,20 @@ export interface Gas {
 }
 
 export async function getGasFee(provider: ethers.providers.JsonRpcProvider): Promise<Gas> {
-  let fee: any = {};
   try {
-    fee = await provider.send('eth_maxPriorityFeePerGas', []);
+    const [fee, block] = await provider.send('eth_maxPriorityFeePerGas', []);
+    const tip = ethers.BigNumber.from(fee);
+    const buffer = tip.div(100).mul(13);
+    const maxPriorityFeePerGas = tip.add(buffer);
+    const maxFeePerGas =
+      block.baseFeePerGas != null ? block.baseFeePerGas.mul(2).add(maxPriorityFeePerGas) : maxPriorityFeePerGas;
+
+    return { maxFeePerGas, maxPriorityFeePerGas };
   } catch (err) {
     console.warn(
       "getGas: eth_maxPriorityFeePerGas failed, falling back to legacy gas price."
     );
-    fee = await provider.getGasPrice();
+    const gas = await provider.getGasPrice();
+    return { maxFeePerGas: gas, maxPriorityFeePerGas: gas };
   }
-  const block = await provider.getBlock('latest');
-  const tip = ethers.BigNumber.from(fee);
-  const buffer = tip.div(100).mul(13);
-  const maxPriorityFeePerGas = tip.add(buffer);
-  const maxFeePerGas =
-    block.baseFeePerGas != null ? block.baseFeePerGas.mul(2).add(maxPriorityFeePerGas) : maxPriorityFeePerGas;
-
-  return { maxFeePerGas, maxPriorityFeePerGas };
 }
