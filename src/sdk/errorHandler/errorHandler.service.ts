29ef1322b2d6dfc7b37492c3b31adec91857a450
Vignesh <vignesh@pillarproject.io>
2024-02-29 14:58:15 +0530

    changed to message from possibleSolution

--- src/sdk/errorHandler/errorHandler.service.ts
+++ src/sdk/errorHandler/errorHandler.service.ts
@@ -1,19 +1,17 @@
 import { entryPointErrorMsg, errorMsg } from "./constants";
 
 export class ErrorHandler extends Error {
-    public possibleSolution: string = null;
+    public rawError: string = null;
     constructor(public error: string, public code?: number) {
         super(error);
-        this.error = error;
+        this.rawError = error;
         this.code = code;
         if (code) {
-            this.possibleSolution = errorMsg[code.toString()];
-            // if (error.includes('AA33 reverted')) {
-            //     this.possibleSolution += ' If using a token, make sure that approval transaction is done for the requested operation and have enough tokens to spend for this transaction.'
-            // }
+            this.message = errorMsg[code.toString()];
             if (entryPointErrorMsg[error]) {
-                this.possibleSolution = entryPointErrorMsg[error];
+                this.message = entryPointErrorMsg[error];
             }
+            
         }
     }
 }
