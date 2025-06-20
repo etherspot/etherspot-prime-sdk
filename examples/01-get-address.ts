292630ec211bf4522d306f52095c9f42f4613f19
Vignesh <vignesh@pillarproject.io>
2025-06-20 14:59:28 +0530

    removed secrets

--- examples/01-get-address.ts
+++ examples/01-get-address.ts
@@ -6,17 +6,13 @@ dotenv.config();
 
 
 async function main() {
-  const bundlerApiKey = '***REMOVED***';
+  const bundlerApiKey = 'etherspot_public_key';
   // initializating sdk...
-  const primeSdk = new PrimeSdk({ privateKey: "***REMOVED***" }, { chainId: Number(137), bundlerProvider: new EtherspotBundler(137, bundlerApiKey) }) // Testnets dont need apiKey on bundlerProvider
+  const primeSdk = new PrimeSdk({ privateKey: process.env.WALLET_PRIVATE_KEY }, { chainId: Number(137), bundlerProvider: new EtherspotBundler(137, bundlerApiKey) }) // Testnets dont need apiKey on bundlerProvider
 
   // get EtherspotWallet address...
   const address: string = await primeSdk.getCounterFactualAddress();
   console.log('\x1b[33m%s\x1b[0m', `EtherspotWallet address: ${address}`);
-
-  const provider = new ethers.providers.JsonRpcProvider(`https://rpc.etherspot.io/v1/100?api-key=${bundlerApiKey}`);
-  const balance = await provider.getBalance(address);
-  console.log('\x1b[33m%s\x1b[0m', `EtherspotWallet balance: ${ethers.utils.formatEther(balance)} MATIC`);
 }
 
 main()
