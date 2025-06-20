278d61c5350f299309b391fd474fb2ab61d9c895
Vignesh <vignesh@pillarproject.io>
2025-06-20 15:15:35 +0530

    fixed lint issue

--- src/sdk/dto/get-step-transactions-lifi.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@lifi/sdk';
-import { IsAddress } from './validators';
-
-export class GetStepTransactionsLiFiDto {
-    route: Route
-
-    @IsAddress()
-    account: string;
-}
